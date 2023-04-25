cff4a3e4339ed74424193701b149e8ea10212187
Richard King <richrdkng@gmail.com>
2023-04-25 20:56:11 +0200

    fix(build): keep hashbang line for bin/index.js during build

--- rollup.config.js
+++ rollup.config.js
@@ -1,19 +1,11 @@
+const { merge }    = require('lodash')
 const { globSync } = require('glob')
 const typescript   = require('@rollup/plugin-typescript')
 
 /** @type {import('rollup').RollupOptions} */
-module.exports = {
-  input: globSync([
-    'bin/**/*.ts',
-    'src/**/*.ts',
-  ], {
-    ignore: [
-      'bin/**/*.test.ts',
-      'src/**/*.test.ts',
-    ],
-  }),
-
+const options = {
   output: {
+    preserveModulesRoot: __dirname,
     preserveModules: true,
 
     sourcemap: true,
@@ -26,3 +18,24 @@ module.exports = {
     typescript(),
   ],
 }
+
+/** @type {import('rollup').RollupOptions[]} */
+module.exports = [
+  merge(
+    {
+      input: globSync('bin/**/*.ts', { ignore: 'bin/**/*.test.ts' }),
+    },
+    {
+      output: {
+        banner: '#!/usr/bin/env node',
+      },
+    },
+    options,
+  ),
+  merge(
+    {
+      input: globSync('src/**/*.ts', { ignore: 'src/**/*.test.ts' }),
+    },
+    options,
+  )
+]
