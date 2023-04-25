762c3323bcbd9b5d0cdcb9dbb9229f8735ce9b58
Richard King <richrdkng@gmail.com>
2023-04-25 16:10:24 +0200

    chore: initial commit

--- jest.config.js
+++ jest.config.js
@@ -3,4 +3,16 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   testTimeout: 30000,
+  // coverageThreshold: {
+  //   global: {
+  //     branches: 100,
+  //     functions: 100,
+  //     lines: 100,
+  //     statements: 100
+  //   }
+  // },
+  coverageReporters: [
+    'lcov',
+    'text'
+  ],
 }
--- rollup.config.js
+++ rollup.config.js
@@ -1,10 +1,36 @@
-const typescript = require('@rollup/plugin-typescript')
+const { globSync } = require('glob')
+const typescript   = require('@rollup/plugin-typescript')
+
+console.log(
+  globSync([
+    'bin/**/*.ts',
+    'src/**/*.ts',
+  ], {
+    ignore: [
+      'bin/**/*.test.ts',
+      'src/**/*.test.ts',
+    ],
+  })
+)
 
 /** @type {import('rollup').RollupOptions} */
 module.exports = {
-  input: 'src/index.ts',
+
+  // input: 'src/index.ts',
+
+  input: globSync([
+    'bin/**/*.ts',
+    'src/**/*.ts',
+  ], {
+    ignore: [
+      'bin/**/*.test.ts',
+      'src/**/*.test.ts',
+    ],
+  }),
 
   output: {
+    preserveModules: true,
+
     sourcemap: true,
     format:    'cjs',
 
